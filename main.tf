081be070b29ba286b62409e0b36f4f2e56281fc8
Youming Lin <youming-lin@users.noreply.github.com>
2020-12-18 16:59:00 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">= 1.17.0"
+    }
+  }
+}
+
 resource "null_resource" "test" {
+  triggers = {
+    timestampe = timestamp()
+  }
   provisioner "local-exec" {
     command = "env && ibmcloud target"
   }
